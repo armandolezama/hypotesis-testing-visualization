eb5f62b18a57c6a57e8ae92825ce41c3003d927e
armandolezama <armando.lezama.psic@gmail.com>
2023-09-04 23:04:01 -0600

    fix: (py): added module syntax for analyzer method

--- modules/analysis.py
+++ modules/analysis.py
@@ -1,5 +1,4 @@
 import scikit_posthocs as sp
-import numpy as np
 from scipy import stats
 import pandas as pd
 pd.options.display.float_format = '{:,.4f}'.format
@@ -9,7 +8,7 @@ class testing_analyzer:
   def __init__(self):
     print(self)
 
-  def check_normality(data):
+  def check_normality(self, data):
     test_stat_normality, p_value_normality=stats.shapiro(data)
     print("p value:%.4f" % p_value_normality)
     if p_value_normality <0.05:
@@ -17,7 +16,7 @@ class testing_analyzer:
     else:
       print("Fail to reject null hypothesis >> The data is normally distributed")       
 
-  def check_variance_homogeneity(group1, group2):
+  def check_variance_homogeneity(self, group1, group2):
     test_stat_var, p_value_var= stats.levene(group1,group2)
     print("p value:%.4f" % p_value_var)
     if p_value_var <0.05:
