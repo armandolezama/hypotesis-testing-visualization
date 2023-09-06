502bb6cd88c4122b615fe12b915ee95fc3f397b4
armandolezama <armando.lezama.psic@gmail.com>
2023-09-05 23:32:12 -0600

    feat(py): added normal distribution simulation

--- modules/data_generator.py
+++ modules/data_generator.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import stats
 
 class data_generator:
   def __init__(self) -> None:
@@ -8,3 +9,8 @@ class data_generator:
         'asyncr' : np.array([77.1, 71.7, 91. , 72.2, 74.8, 85.1, 67.6, 69.9, 75.3, 71.7, 65.7, 72.6, 71.5, 78.2]),
       }
     }
+  
+  def get_normal_data_simulation(self, data, sample_size):
+    mean, std_dev = stats.norm.fit(data)
+    return np.random.normal(loc=mean, scale=std_dev, size=sample_size)
+  
