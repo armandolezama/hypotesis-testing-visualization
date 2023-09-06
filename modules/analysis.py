97e0c09e227dafca0b06199da8c738f011c60436
armandolezama <armando.lezama.psic@gmail.com>
2023-09-05 22:28:16 -0600

    feat(py): added independent t test function

--- modules/analysis.py
+++ modules/analysis.py
@@ -23,3 +23,12 @@ class testing_analyzer:
       print("Reject null hypothesis >> The variances of the samples are different.")
     else:
       print("Fail to reject null hypothesis >> The variances of the samples are same.")
+  
+  def run_t_test_independent(self, sample1, sample2):
+    ttest,p_value = stats.ttest_ind(sample1,sample2)
+    print("p value:%.8f" % p_value)
+    print("since the hypothesis is one sided >> use p_value/2 >> p_value_one_sided:%.4f" %(p_value/2))
+    if p_value/2 <0.05:
+        print("Reject null hypothesis")
+    else:
+        print("Fail to reject null hypothesis") 
